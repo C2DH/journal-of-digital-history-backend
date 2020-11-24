ffcb24f46a61bc556c4ef3b55c9e696e4ebd249b
eliselavy <eliselavy@gmail.com>
2020-11-24 15:57:38 +0100

    addition env

--- jdh/settings.py
+++ jdh/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_env_variable('DEBUG', 'True') == 'True'
