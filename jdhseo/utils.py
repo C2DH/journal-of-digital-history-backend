1ddeca1383603fe07ad8b023a870cf57d2ba5ce4
Daniele Guido <gui.daniele@gmail.com>
2021-07-12 15:40:35 +0200

    add method parseJupyterNotebook (first rough version)

---
+++ jdhseo/utils.py
@@ -0,0 +1,33 @@
+import marko
+
+
+def parseJupyterNotebook(notebook):
+    cells = notebook.get('cells')
+    title = []
+    abstract = []
+    contributor = []
+    disclaimer = []
+    paragraphs = []
+    for cell in cells:
+        # check cell metadata
+        tags = cell.get('metadata', {}).get('tags', [])
+        source = ''.join(cell.get('source', ''))
+        if 'hidden' in tags:
+            continue
+        if 'title' in tags:
+            title.append(marko.convert(source))
+        elif 'abstract' in tags:
+            abstract.append(marko.convert(source))
+        elif 'contributor' in tags:
+            contributor.append(marko.convert(source))
+        elif 'disclaimer' in tags:
+            disclaimer.append(marko.convert(source))
+        elif cell.get('cell_type') == 'markdown':
+            paragraphs.append(marko.convert(source))
+    return {
+        'title': title,
+        'abstract': abstract,
+        'contributor': contributor,
+        'disclaimer': disclaimer,
+        'paragraphs': paragraphs
+    }
