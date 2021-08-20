1862b488ce188d3a04632c4620709826c1bf4969
Daniele Guido <gui.daniele@gmail.com>
2021-08-20 16:30:49 +0200

    add action to ArticleAdmin

--- jdhapi/admin.py
+++ jdhapi/admin.py
@@ -2,18 +2,31 @@ import os
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Author, Abstract, Dataset, Article, Issue, Tag, Role
+from .tasks import save_article_fingerprint
+
+
+def save_notebook_fingerprint(modeladmin, request, queryset):
+    for article in queryset:
+        save_article_fingerprint.delay(article_id=article.pk)
+
+
+save_notebook_fingerprint.short_description = "Save notebook fingerprint in data"
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['issue', 'status']
+    actions = [save_notebook_fingerprint]
+
 
 # Register your models here.
 admin.site.register(Abstract)
 admin.site.register(Dataset)
 admin.site.register(Author)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Issue)
 admin.site.register(Tag)
 admin.site.register(Role)
-
 admin.site.site_url = "/dashboard"
-
 admin.site.site_header = mark_safe(
     '<b style="color:white">JDH admin</b>'
     f'({os.environ.get("GIT_BRANCH")}/{os.environ.get("GIT_REVISION")})'
