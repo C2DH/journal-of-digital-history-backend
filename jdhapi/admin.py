f496b62bb7c49249c96fa3fc33961f5a4746d4ed
Daniele Guido <gui.daniele@gmail.com>
2021-08-26 13:58:23 +0200

    Fix/add fingerprint issue 26 (#35)
    * Update Pipfile
    
    * Update Pipfile.lock
    
    * create util function for Article model
    
    * add save_article_fingerprint task to save fingerprint conveniently
    
    * add action to ArticleAdmin
    
    * create command to get the fingerprint for an article (doesn't save!)
    
    * Update article.py
    
    * addition filter - article title
    
    * addition JSON field to article model for fingerprint/citation - use full raw url notebook_ipython_url
    
    Co-authored-by: eliselavy <eliselavy@gmail.com>

--- jdhapi/admin.py
+++ jdhapi/admin.py
@@ -2,18 +2,41 @@ import os
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Author, Abstract, Dataset, Article, Issue, Tag, Role
+from .tasks import save_article_fingerprint
+
+
+def save_notebook_fingerprint(modeladmin, request, queryset):
+    for article in queryset:
+        save_article_fingerprint.delay(article_id=article.pk)
+
+
+save_notebook_fingerprint.short_description = "Save notebook fingerprint in data"
+
+
+class AbstractAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ('status',)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['issue', 'abstract_title', 'status']
+    list_filter = ('issue', 'status')
+    actions = [save_notebook_fingerprint]
+
+    def abstract_title(self, obj):
+        return obj.abstract.title
+    abstract_title.short_description = 'Title'
+
 
 # Register your models here.
-admin.site.register(Abstract)
+admin.site.register(Abstract, AbstractAdmin)
 admin.site.register(Dataset)
 admin.site.register(Author)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Issue)
 admin.site.register(Tag)
 admin.site.register(Role)
-
 admin.site.site_url = "/dashboard"
-
 admin.site.site_header = mark_safe(
     '<b style="color:white">JDH admin</b>'
     f'({os.environ.get("GIT_BRANCH")}/{os.environ.get("GIT_REVISION")})'
--- jdhapi/models/article.py
+++ jdhapi/models/article.py
@@ -1,8 +1,4 @@
 from django.db import models
-from django.conf import settings
-from django.utils import timezone
-from django.db.models import JSONField
-import shortuuid
 
 
 class Article(models.Model):
@@ -23,8 +19,12 @@ class Article(models.Model):
     )
 
     repository_url = models.URLField(max_length=254, null=True, blank=True)
+    # Url of the article in the front
     notebook_url = models.CharField(max_length=254, null=True, blank=True)
-    notebook_commit_hash = models.CharField(max_length=22, default='', help_text='store the git hash', blank=True)
+    # Json source from raw.github
+    notebook_ipython_url = models.URLField(max_length=254, null=True, blank=True)
+    notebook_commit_hash = models.CharField(
+        max_length=22, default='', help_text='store the git hash', blank=True)
     notebook_path = models.CharField(max_length=254, null=True, blank=True)
     binder_url = models.URLField(max_length=254, null=True, blank=True)
     status = models.CharField(
@@ -38,7 +38,15 @@ class Article(models.Model):
         default=RepositoryType.GITHUB,
     )
     issue = models.ForeignKey('jdhapi.Issue', on_delete=models.CASCADE)
-    data = models.JSONField(verbose_name=u'data contents', help_text='JSON format', default=dict, blank=True)
+    data = models.JSONField(
+        verbose_name=u'data contents', help_text='JSON format',
+        default=dict, blank=True)
+    fingerprint = models.JSONField(
+        verbose_name=u'fingerprint contents', help_text='JSON format',
+        default=dict, blank=True)
+    citation = models.JSONField(
+        verbose_name=u'citation contents', help_text='JSON format',
+        default=dict, blank=True)
     tags = models.ManyToManyField('jdhapi.Tag', blank=True)
     authors = models.ManyToManyField('jdhapi.Author', through='Role')
 
