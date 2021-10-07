c007947d67cb8ef118aa5be43391215fa14e3a05
eliselavy <eliselavy@gmail.com>
2021-10-07 14:33:28 +0200

    by using SEO template

--- jdh/views.py
+++ jdh/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from .utils import render_to_pdf, generate_qrcode
 from django.template.loader import get_template
 from xhtml2pdf import pisa
+import base64
 import os
 import logging
 import os
@@ -10,6 +11,9 @@ from django.conf import settings
 from django.contrib.staticfiles import finders
 from jdhapi.models import Abstract, Article
 from django.shortcuts import render, get_object_or_404
+import urllib.parse
+from jdhseo.utils import parseJupyterNotebook
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -18,21 +22,35 @@ class GeneratePDF(View):
     def get(self, request, pid, *args, **kwargs):
         abstract = get_object_or_404(Abstract, pid=pid)
         article = get_object_or_404(Article, abstract=abstract)
-        template_file = 'abstract.html'
+        template_file = 'article_detail.html'
         template = get_template(template_file)
         abstract_text = article.data['abstract'][0]
         first_caracter = abstract_text[0:1]
         qrCodebase64 = generate_qrcode(pid)
         context = {
-            "article_title": abstract.title,
-            "article_authors": abstract.contact_firstname + " " + abstract.contact_lastname,
-            "article_authors_affiliation": abstract.contact_affiliation,
-            "article_keywords": article.data['keywords'][0],
-            "article_abstract_first_letter": first_caracter,
-            "article_abstract": abstract_text[1:],
-            "qr_code": qrCodebase64
-
+            'article': article,
+            'qr_code': qrCodebase64
         }
+        # decode notebook url
+        notebook_url = urllib.parse.unquote(
+            base64.b64decode(article.notebook_url).decode('utf-8'))
+        # check if it is a github url
+
+        context.update({'proxy': 'github', 'host': settings.JDHSEO_PROXY_HOST})
+        if notebook_url.startswith(settings.JDHSEO_PROXY_PATH_GITHUB):
+            # load contents from remote link (it uses nginx cache if availeble.
+            # this can be improved using local cacle mechanism
+            # Cf. https://docs.djangoproject.com/en/3.2/topics/cache/
+            remote_url = urllib.parse.urljoin(
+                settings.JDHSEO_PROXY_HOST, notebook_url)
+            try:
+                res = requests.get(remote_url)
+                # add NB paragraphs to context
+                context.update({'nb': parseJupyterNotebook(res.json())})
+            except Exception as e:
+                logger.error(f'Error occurred on article pk={article.pk} notebook')
+                logger.exception(e)
+                raise Http404("Article Notebook file not found (or not valid)")
         html = template.render(context)
         pdf = render_to_pdf(template_file, context)
         if pdf:
@@ -46,6 +64,7 @@ class GeneratePDF(View):
             return response
             # return HttpResponse(pdf, content_type='application/pdf')
         return HttpResponse("Not found")
+
 
 
 """def get(self, request, *args, **kwargs):
--- jdhseo/utils.py
+++ jdhseo/utils.py
@@ -1,4 +1,41 @@
 import marko
+import logging
+import base64
+import qrcode
+from io import BytesIO
+from citeproc import formatter
+from citeproc import Citation
+from citeproc import CitationItem
+from citeproc import CitationStylesStyle
+from citeproc import CitationStylesBibliography
+from citeproc.source.json import CiteProcJSON
+
+logger = logging.getLogger(__name__)
+
+
+def getReferencesFromJupyterNotebook(notebook):
+    metadata = notebook.get('metadata')
+    references = []
+    bibliography = []
+    try:
+        references = metadata.get('cite2c').get('citations')
+        bib_source = CiteProcJSON(references.values())
+        bib_style = CitationStylesStyle(
+            'jdhseo/styles/modern-language-association.csl', validate=False)
+        bib = CitationStylesBibliography(
+            bib_style, bib_source, formatter.html)
+        # register citation
+        for key, entry in bib_source.items():
+            # print(key)
+            # for name, value in entry.items():
+            #     print('   {}: {}'.format(name, value))
+            bib.register(Citation([CitationItem(key)]))
+        for item in bib.bibliography():
+            bibliography.append(str(item))
+    except Exception as e:
+        logger.exception(e)
+        pass
+    return references, bibliography
 
 
 def parseJupyterNotebook(notebook):
@@ -8,6 +45,10 @@ def parseJupyterNotebook(notebook):
     contributor = []
     disclaimer = []
     paragraphs = []
+    collaborators = []
+    keywords = []
+    references, bibliography = getReferencesFromJupyterNotebook(notebook)
+
     for cell in cells:
         # check cell metadata
         tags = cell.get('metadata', {}).get('tags', [])
@@ -22,12 +63,21 @@ def parseJupyterNotebook(notebook):
             contributor.append(marko.convert(source))
         elif 'disclaimer' in tags:
             disclaimer.append(marko.convert(source))
+        elif 'collaborators' in tags:
+            collaborators.append(marko.convert(source))
+        elif 'keywords' in tags:
+            keywords.append(marko.convert(source))
         elif cell.get('cell_type') == 'markdown':
             paragraphs.append(marko.convert(source))
+
     return {
         'title': title,
         'abstract': abstract,
         'contributor': contributor,
         'disclaimer': disclaimer,
-        'paragraphs': paragraphs
+        'paragraphs': paragraphs,
+        'collaborators': collaborators,
+        'keywords': keywords,
+        'references': references,
+        'bibliography': bibliography
     }
