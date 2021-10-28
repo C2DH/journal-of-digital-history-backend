a346db2c7ce20ad019cb234ad3bd4460e57922b9
Daniele Guido <gui.daniele@gmail.com>
2021-10-28 17:26:25 +0200

    still todo the celery task

--- jdhtasks/tasks.py
+++ jdhtasks/tasks.py
@@ -7,3 +7,8 @@ logger = logging.getLogger(__name__)
 @app.task(bind=True)
 def echo(self, message):
     logger.info(f'Your message received: {message}')
+
+
+@app.task(bind=True)
+def store(self, pid):
+    logger.info(f'Store pid={pid}')
