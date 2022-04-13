aa2d6756036e3c55c3f1aa37c6ddfb6c3d45bee0
eliselavy <35602279+eliselavy@users.noreply.github.com>
2022-04-13 16:41:39 +0200

    Hotfix 116 (#117)
    * Update utils.py
    
    * Update utils.py

--- jdhseo/utils.py
+++ jdhseo/utils.py
@@ -113,7 +113,10 @@ def parseJupyterNotebook(notebook, contact_orcid):
         elif 'abstract' in tags:
             abstract.append(marko.convert(source))
         elif 'contributor' in tags:
-            contributor.append(marko.convert(source + ' - ' + affiliation))
+            if affiliation:
+                contributor.append(marko.convert(source + ' - ' + affiliation))
+            else:
+                contributor.append(marko.convert(source))
         elif 'disclaimer' in tags:
             disclaimer.append(marko.convert(source))
         elif 'collaborators' in tags:
@@ -222,17 +225,20 @@ def get_affiliation(orcid):
 # URL https://doi.org/10.1515/JDH-2021-1006
 # stops are replaced by hyphens and the Revision number is removed,
 def getDoiUrlDGFormatted(doi):
-    DOI_URL = "https://doi.org/"
     doi_all = ""
-    doi_group = re.split('/', doi)
-    for index, element in enumerate(doi_group):
-        match = re.search('JDH', element)
-        if index == 0:
-            doi_all = DOI_URL + element
-        else:
-            if match:
-                hyphen = element.replace(".", "-")
-                doi_all = doi_all + "/" + hyphen.rsplit('-', 1)[0]
-                return doi_all
+    if doi:
+        DOI_URL = "https://doi.org/"
+        doi_group = re.split('/', doi)
+        for index, element in enumerate(doi_group):
+            match = re.search('JDH', element)
+            if index == 0:
+                doi_all = DOI_URL + element
             else:
-                return doi_all
+                if match:
+                    hyphen = element.replace(".", "-")
+                    doi_all = doi_all + "/" + hyphen.rsplit('-', 1)[0]
+                    return doi_all
+                else:
+                    return doi_all
+    else:
+        return doi_all
