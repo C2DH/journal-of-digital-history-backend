c2d8188ff1ebba34b9e9da9f0ec152b46a6c07f5
eliselavy <eliselavy@gmail.com>
2022-04-13 16:40:42 +0200

    Update utils.py

--- jdhseo/utils.py
+++ jdhseo/utils.py
@@ -225,17 +225,20 @@ def get_affiliation(orcid):
 # URL https://doi.org/10.1515/JDH-2021-1006
 # stops are replaced by hyphens and the Revision number is removed,
 def getDoiUrlDGFormatted(doi):
-    DOI_URL = "https://doi.org/"
     doi_all = ""
-    doi_group = re.split('/', doi)
-    for index, element in enumerate(doi_group):
-        match = re.search('JDH', element)
-        if index == 0:
-            doi_all = DOI_URL + element
-        else:
-            if match:
-                hyphen = element.replace(".", "-")
-                doi_all = doi_all + "/" + hyphen.rsplit('-', 1)[0]
-                return doi_all
+    if doi:
+        DOI_URL = "https://doi.org/"
+        doi_group = re.split('/', doi)
+        for index, element in enumerate(doi_group):
+            match = re.search('JDH', element)
+            if index == 0:
+                doi_all = DOI_URL + element
             else:
-                return doi_all
+                if match:
+                    hyphen = element.replace(".", "-")
+                    doi_all = doi_all + "/" + hyphen.rsplit('-', 1)[0]
+                    return doi_all
+                else:
+                    return doi_all
+    else:
+        return doi_all
