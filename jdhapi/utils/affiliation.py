eddd24dc38c7ffbafd8fe0fcd83d7400f468c154
eliselavy <eliselavy@gmail.com>
2022-08-25 16:30:51 +0200

    addition pycountry

--- jdhapi/utils/affiliation.py
+++ jdhapi/utils/affiliation.py
@@ -1,4 +1,5 @@
 import logging
+import pycountry
 from jdhseo.utils import get_affiliation
 logger = logging.getLogger(__name__)
 
@@ -22,18 +23,21 @@ def get_affiliation_json_one(orcid_url, affiliation):
     orcid = orcid_url.partition(ORCID_URL)[2]
     city_country = get_affiliation(orcid)
     if city_country.find('-') != -1:
-        city = city_country.partition('-')[0]
-        country = city_country.partition('-')[2]
+        city = city_country.partition('-')[0].strip()
+        country = city_country.partition('-')[2].strip()
+        country_name = pycountry.countries.get(alpha_2=country).name
         affiliation = {
             "institution": affiliation,
             "city": city,
-            "country": country
+            "country": country,
+            "country_name": country_name
         }
     else:
         affiliation = {
             "institution": affiliation,
             "city": "NOT FOUND",
-            "country": "NOT FOUND"
+            "country": "NOT FOUND",
+            "country_name": "NOT FOUND"
         }
     return affiliation
 
