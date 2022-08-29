c24d2c425e8de15c8077f6513a270adb695ce1d5
eliselavy <eliselavy@gmail.com>
2022-08-29 13:57:07 +0200

    fix if no orcid

--- jdhapi/utils/affiliation.py
+++ jdhapi/utils/affiliation.py
@@ -25,27 +25,31 @@ def get_authors(article_authors, affiliations):
 
 
 def get_affiliation_json_one(orcid_url, affiliation):
-    # contact_orcid
-    ORCID_URL = "https://orcid.org/"
-    orcid = orcid_url.partition(ORCID_URL)[2]
-    city_country = get_affiliation(orcid)
-    if city_country.find('-') != -1:
-        city = city_country.partition('-')[0].strip()
-        country = city_country.partition('-')[2].strip()
-        country_name = pycountry.countries.get(alpha_2=country).name
-        affiliation = {
-            "institution": affiliation,
-            "city": city,
-            "country": country,
-            "country_name": country_name
-        }
+    default_affiliation = {
+        "institution": affiliation,
+        "city": "NOT FOUND",
+        "country": "NOT FOUND",
+        "country_name": "NOT FOUND"
+    }
+    if orcid_url:
+        # contact_orcid
+        ORCID_URL = "https://orcid.org/"
+        orcid = orcid_url.partition(ORCID_URL)[2]
+        city_country = get_affiliation(orcid)
+        if city_country.find('-') != -1:
+            city = city_country.partition('-')[0].strip()
+            country = city_country.partition('-')[2].strip()
+            country_name = pycountry.countries.get(alpha_2=country).name
+            affiliation = {
+                "institution": affiliation,
+                "city": city,
+                "country": country,
+                "country_name": country_name
+            }
+        else:
+            affiliation = default_affiliation
     else:
-        affiliation = {
-            "institution": affiliation,
-            "city": "NOT FOUND",
-            "country": "NOT FOUND",
-            "country_name": "NOT FOUND"
-        }
+        return default_affiliation
     return affiliation
 
 
