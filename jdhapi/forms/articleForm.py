db94738b0d589201836106e592e613f97bfc3c76
eliselavy <eliselavy@gmail.com>
2022-09-22 10:44:17 +0200

    prevent PEER_REVIEW if no required files

--- jdhapi/forms/articleForm.py
+++ jdhapi/forms/articleForm.py
@@ -1,3 +1,4 @@
+from re import I
 from django import forms
 from jdhapi.models import Article
 from jdhapi.utils.gitup_repository import is_socialmediacover_exist
@@ -8,9 +9,20 @@ class ArticleForm(forms.ModelForm):
         doi = self.cleaned_data['doi']
         status = self.cleaned_data['status']
         repository_url = self.cleaned_data['repository_url']
+        notebook_url = self.cleaned_data['notebook_url']
+        notebook_path = self.cleaned_data['notebook_path']
+        binder_url = self.cleaned_data['binder_url']
+        notebook_ipython_url = self.cleaned_data['notebook_ipython_url']
         if status == Article.Status.PUBLISHED:
             if not doi:
                 raise forms.ValidationError({'doi': "Doi is mandatory if published"})
             if not is_socialmediacover_exist(repository_url):
                 raise forms.ValidationError({'repository_url': "No social media cover uploaded in the GitHub repository"})
+        if status == Article.Status.PEER_REVIEW:
+            if not repository_url:
+                raise forms.ValidationError({'repository_url': "Repository_url is mandatory if published"})
+            if not notebook_url:
+                raise forms.ValidationError({'notebook_url': "Notebook_url is mandatory if published"})
+            if not notebook_path:
+                raise forms.ValidationError({'notebook_path': "Notebook_url is mandatory if published"})
 
--- jdhapi/utils/gitup_repository.py
+++ jdhapi/utils/gitup_repository.py
@@ -7,12 +7,15 @@ logger = logging.getLogger(__name__)
 
 def is_socialmediacover_exist(repository_url):
     # Get Url
-    url = repository_url + "/blob/main/socialmediacover.png"
-    get = requests.get(url)
-    # if the request succeeds
-    if get.status_code == 200:
-        logger.info(f"{url}: is reachable")
-        return True
+    if repository_url:
+        url = repository_url + "/blob/main/socialmediacover.png"
+        get = requests.get(url)
+        # if the request succeeds
+        if get.status_code == 200:
+            logger.info(f"{url}: is reachable")
+            return True
+        else:
+            logger.info(f"{url}: is Not reachable, status_code: {get.status_code}")
+            return False
     else:
-        logger.info(f"{url}: is Not reachable, status_code: {get.status_code}")
         return False
