2ca60555d8fa56f3cf546fa94036d8e633ed0c94
eliselavy <eliselavy@gmail.com>
2022-10-03 11:44:32 +0200

    is_socialmediacover_exist png / jpg

--- jdhapi/utils/gitup_repository.py
+++ jdhapi/utils/gitup_repository.py
@@ -5,17 +5,29 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def is_reachable(url):
+    get = requests.get(url)
+    if get.status_code == 200:
+        logger.info(f"{url}: is reachable")
+        return True
+    else:
+        logger.info(f"{url}: is Not reachable, status_code: {get.status_code}")
+        return False
+
+
 def is_socialmediacover_exist(repository_url):
     # Get Url
     if repository_url:
-        url = repository_url + "/blob/main/socialmediacover.png"
-        get = requests.get(url)
+        url_1 = repository_url + "/blob/main/socialmediacover.png"
+        url_2 = repository_url + "/blob/main/socialmediacover.jpg"
+        get_1 = requests.get(url_1)
         # if the request succeeds
-        if get.status_code == 200:
-            logger.info(f"{url}: is reachable")
+        if is_reachable(url_1):
             return True
         else:
-            logger.info(f"{url}: is Not reachable, status_code: {get.status_code}")
-            return False
+            if is_reachable(url_2):
+                return True
+            else:
+                return False
     else:
         return False
