3024ab2c134a7eeb0feead02415bc933999d4d43
eliselavy <eliselavy@gmail.com>
2025-06-16 15:16:31 +0200

    SessionAuthentication

--- jdh/settings.py
+++ jdh/settings.py
@@ -73,6 +73,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
