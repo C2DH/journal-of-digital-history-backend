d6e30bb22ff5a5990c014e5af7f8f95e7d643730
Yaroslav <yaro3245@gmail.com>
2025-07-10 16:39:13 +0200

    Search unit test & reverting to the "/" after keyword #288 #291

--- jdhapi/urls.py
+++ jdhapi/urls.py
@@ -12,24 +12,24 @@ urlpatterns = [
     path(
         "api/datasets/<int:pk>/", views.DatasetDetail.as_view(), name="dataset-detail"
     ),
-    re_path(r"^api/abstracts/?$", views.AbstractList.as_view(), name="abstract-list"),
-    # path("api/articles/", views.ArticleList.as_view(), name="article-list"),
-    # path(
-    #     "api/articles/<str:abstract__pid>/",
-    #     views.ArticleDetail.as_view(),
-    #     name="article-detail",
-    # ),
-    re_path(
-        r"^api/articles/?$",
-        views.ArticleList.as_view(),
-        name="article-list",
-    ),
-    # detail view: matches '/api/articles/<pid>' and '/api/articles/<pid>/'
-    re_path(
-        r"^api/articles/(?P<abstract__pid>[^/]+)/?$",
+    path("api/abstracts/", views.AbstractList.as_view(), name="abstract-list"),
+    path("api/articles/", views.ArticleList.as_view(), name="article-list"),
+    path(
+        "api/articles/<str:abstract__pid>/",
         views.ArticleDetail.as_view(),
         name="article-detail",
     ),
+    # re_path(
+    #     r"^api/articles/?$",
+    #     views.ArticleList.as_view(),
+    #     name="article-list",
+    # ),
+    # # detail view: matches '/api/articles/<pid>' and '/api/articles/<pid>/'
+    # re_path(
+    #     r"^api/articles/(?P<abstract__pid>[^/]+)/?$",
+    #     views.ArticleDetail.as_view(),
+    #     name="article-detail",
+    # ),
     path("api/csrf/", views.get_csrf, name="get_csrf"),
     path("api/issues/", views.IssueList.as_view(), name="issue-list"),
     path("api/issues/<str:pid>/", views.IssueDetail.as_view(), name="issue-detail"),
