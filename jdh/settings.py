e88acf10a4bab8b7f4dd2ee0adddae46d17764f9
eliselavy <eliselavy@gmail.com>
2025-09-04 10:21:19 +0200

    https://github.com/C2DH/journal-of-digital-history-ipynb-preflight-action/issues/57

--- jdh/settings.py
+++ jdh/settings.py
@@ -67,6 +67,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
