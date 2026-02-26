5a58d8b17e64b3011f513ed4e0bd9781c94924c8
salaun.marion <marion.salaun@uni.lu>
2026-02-26 16:04:13 +0100

    (fix) removed flake8 linter for ruff configuration pyproject.yml

--- jdhapi/signals.py
+++ jdhapi/signals.py
@@ -1,11 +1,10 @@
 import requests
-import logging
 from django.core.exceptions import ValidationError
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
+
 from jdhapi.models import Article
 from jdhapi.utils.articles import convert_string_to_base64
-
 
 
 @receiver(pre_save, sender=Article)
@@ -37,5 +36,4 @@ def validate_urls_for_article_submission(sender, instance, **kwargs):
     if instance.notebook_url and check_notebook_url(
         instance.notebook_url, instance.repository_url
     ):
-
         raise ValidationError("Notebook url is not correct")
