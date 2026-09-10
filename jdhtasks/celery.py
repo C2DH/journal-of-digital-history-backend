8e8bf6f974cceeea89ce9661a369264008d1045b
salaun.marion <marion.salaun@uni.lu>
2026-09-10 16:39:40 +0200

    fix(celery): config add broker and backend localhost

--- jdhtasks/celery.py
+++ jdhtasks/celery.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jdh.settings")
-app = Celery("jdhtasks")
+app = Celery("jdhtasks", broker="redis://localhost:6379", backend="redis://localhost:6379")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
